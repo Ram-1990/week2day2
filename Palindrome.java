68497740c5a6dfa335105d971494111c1d64b754
Ramyaa <90095424+Ram-1990@users.noreply.github.com>
2021-09-10 00:11:44 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package week2day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+	int num=454;
+	int rev=0;
+	int temp=num;
+	while(num>0) {
+		rev=rev*10;
+		rev=rev+num%10;
+		num=num/10;
+	}
+	if(rev==temp) {
+				System.out.println("Palindrome");
+	}
+	else
+	{System.out.println("Not a Palindrome");
+	}
+
+	}
+
+}
